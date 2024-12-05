5fbd0ad4de5c6df2c168b3e0055e73189cd018db
Gul Imran <gul.imran2@sky.uk>
2024-12-05 20:11:03 +0000

    Skeleton spring boot app with gradle wrapper
    Co-authored-by: Dale Robinson <dale.robinson@sky.uk>

---
+++ src/test/java/imran/exercise/library/management/LibraryManagementApplicationTest.java
@@ -0,0 +1,13 @@
+package imran.exercise.library.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class LibraryManagementApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // Ensure the application context loads successfully.
+    }
+}
